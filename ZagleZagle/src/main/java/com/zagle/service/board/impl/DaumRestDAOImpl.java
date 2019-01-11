8ce3cdc724df12a12d9794b843d47fb75389cf36
SangaChoi <tkddk92@naver.com>
2019-01-11 17:15:24 +0900

    01/11:상아:addBoard Controller 테스트중

--- ZagleZagle/src/main/java/com/zagle/service/board/impl/DaumRestDAOImpl.java
+++ ZagleZagle/src/main/java/com/zagle/service/board/impl/DaumRestDAOImpl.java
@@ -1,9 +1,22 @@
 package com.zagle.service.board.impl;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
 import com.zagle.service.board.RestBoardDAO;
 
+@Repository("daumRestDAOImpl")
 public class DaumRestDAOImpl implements RestBoardDAO{
 
+	@Autowired
+	@Qualifier("sqlSessionTemplate")
+	private SqlSession sqlSession;
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
 	@Override
 	public void shareBoard(String boardNo) throws Exception {
 		// TODO Auto-generated method stub
