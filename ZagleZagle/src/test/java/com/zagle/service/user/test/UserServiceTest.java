79ed84520cba4036f594b20126d71dba5f90aded
Bit <Bit@DESKTOP-H23MQSQ>
2019-01-11 14:59:26 +0900

    1월 10일 임상후 : 회원관리 CRUD 완성 //D는 없음

--- ZagleZagle/src/test/java/com/zagle/service/user/test/UserServiceTest.java
+++ ZagleZagle/src/test/java/com/zagle/service/user/test/UserServiceTest.java
@@ -25,31 +25,74 @@ public class UserServiceTest {
 	@Qualifier("userServiceImpl")
 	private UserService userService;
 	
-	@Test
+	//@Test
 	public void testAddUser() throws Exception {
 		
 		User user = new User();
-		user.setUserNo("US10003");
-		user.setUserName("임상후");
-		user.setPhone("01012345678");
+		user.setUserNo("US10007");
+		user.setSnsNo("G10001");
+		user.setUserName("임상후2");
 		user.setUserNickname("Joey");
-		user.setUserBirth(20001231);
+		user.setPhone("01012345678");
 		user.setUserSex("남");
 		user.setUserAddr("서울시 성동구 상원길38-1");
-		user.setSnsNo("00100");
+		//user.setProfile("cat.jpg");
+		user.setGrade("0");
 		user.setSnsName("Google");
+		//user.setBankName("신한은행");
+		user.setUserBirth(20001231);
+		//user.setAccount("110406555105");
+		
+		
+		
+		
 		
 		userService.addUser(user);
 		
-		user = userService.getUser("US10003");
+		user = userService.getUser("US10007");
 		
 		
-		Assert.assertEquals("US10005", user.getUserNo());
-		Assert.assertEquals("임상후", user.getUserName());
+		Assert.assertEquals("US10007", user.getUserNo());
+		Assert.assertEquals("임상후2", user.getUserName());
 		Assert.assertEquals("01012345678", user.getPhone());
 		
 		
 	}
-
+	
+	//@Test
+	public void testGetUser() throws Exception {
+		
+		User user = new User();
+		
+		user = userService.getUser("US10005");
+		
+		Assert.assertEquals("임상후", user.getUserName());
+	}
+	
+	@Test
+	public void testUpdateUser() throws Exception {
+		
+		User user = userService.getUser("US10005");
+		
+		Assert.assertNotNull(user);
+		
+		Assert.assertEquals("Joey", user.getUserNickname());
+		
+		user.setUserName("림상후");
+		
+		user.setPhone("010999911111");
+		user.setUserNickname("LimStrop");
+		user.setUserBirth(19900101);
+		user.setUserSex("여");
+		user.setUserAddr("서울시 서초구 비트캠프");
+		user.setProfile("ssibal.jpg");
+		
+		
+		userService.updateUser(user);
+		
+		user = userService.getUser("US10005");
+		Assert.assertNotNull(user);
+	}
+	
 
 }
