54fd351fb6f7f6ddacfce3c6165ac358e4a4a35e
bit <bit@DESKTOP-DH91VNL>
2019-01-15 14:06:04 +0900

    01-15 주현 : 푹쉬

--- ZagleZagle/src/main/java/com/zagle/web/stream/StreamRestController.java
+++ ZagleZagle/src/main/java/com/zagle/web/stream/StreamRestController.java
@@ -1,10 +1,14 @@
 package com.zagle.web.stream;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +20,7 @@ import com.zagle.service.stream.StreamService;
 import com.zagle.service.user.UserService;
 
 @Controller
-@RequestMapping("/stream/json")
+@RequestMapping("/stream/json/")
 public class StreamRestController {
 
 	@Autowired
@@ -46,9 +50,30 @@ public class StreamRestController {
 	streamService.addStream(stream);
 	
 	ModelAndView view = new ModelAndView("redirect:localhost:3000/streamer="+user.getUserNo()+"&userName="+user.getUserNickname());
-	return view;
-			
+	return view;	
 	}
 	
-	
+	@RequestMapping(value="payStream",method=RequestMethod.POST)
+	public ModelAndView payStream(@RequestBody String httpParam) throws Exception{
+		System.out.println("payStream====");
+ ModelAndView modelandview = new ModelAndView();
+
+        JSONParser jsonParser = new JSONParser();
+
+        JSONArray insertParam = (JSONArray) jsonParser.parse(httpParam);
+
+        for(int i=0; i<insertParam.size(); i++){
+
+            //배열 안에 있는것도 JSON형식 이기 때문에 JSON Object 로 추출
+            JSONObject insertData = (JSONObject) insertParam.get(i);
+            	System.out.println(insertData);
+                modelandview.addObject("result", insertData);
+	}
+
+		return modelandview;
+        
+	}
 }
+	
+	
+	
