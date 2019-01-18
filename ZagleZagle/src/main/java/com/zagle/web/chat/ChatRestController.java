c2e9dced73695ba19ab10dcdccbb5bb35ffed3db
Bit <Bit@DESKTOP-50M3DV8>
2019-01-18 19:47:59 +0900

    1/18 인호 : 이미지,동영상 전송 구현,번역구현

--- ZagleZagle/src/main/java/com/zagle/web/chat/ChatRestController.java
+++ ZagleZagle/src/main/java/com/zagle/web/chat/ChatRestController.java
@@ -1,12 +1,28 @@
 package com.zagle.web.chat;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Iterator;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -54,15 +70,52 @@ public class ChatRestController {
         return fileName;
 	}
 	
-	@RequestMapping(value="json/checkFile/{fileName}",method=RequestMethod.GET)
-	public String checkFile(@PathVariable String fileName) {
-		System.out.println("체크 파일네임!! "+fileName+".jpg");
-		String path = "C:\\Users\\Bit\\git\\GoldSuZo\\ZagleZagle\\WebContent\\common\\images\\chat\\"+fileName+".jpg";
-		File file = new File(path);
-		if (file.exists()) {
-			return "false";
-		}else {
-			return "true";
-		}
+	@RequestMapping(value="json/translate/",method=RequestMethod.POST)
+	public @ResponseBody String transText(@RequestParam("text") String data) throws Exception {
+		String clientId = "om5qIXJJtvBuRNBtOir8";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "aUe54sGqx8";//애플리케이션 클라이언트 시크릿값";
+        System.out.println(data+"<<----컨트롤러안에데이터");
+        String text = URLEncoder.encode(data, "UTF-8");
+        StringBuffer response = new StringBuffer();
+        String result="";
+        try {
+            
+            String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            // post request
+            String postParams = "source=ko&target=en&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            
+            JSONObject jsonobj1 = (JSONObject)new JSONParser().parse(br);
+            JSONObject jsonobj2 = (JSONObject)new JSONParser().parse(jsonobj1.get("message").toString());
+            JSONObject jsonobj3 = (JSONObject)new JSONParser().parse(jsonobj2.get("result").toString());
+    		
+            String inputLine;
+            result = (String)jsonobj3.get("translatedText");
+            System.out.println(result);
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("리턴전에 값 : "+result);
+        return result;
 	}
 }
